2eb7a3723d25857f88fef1e4df85131d2c8deb97
chawrona <dawidchawrona@gmail.com>
2025-04-22 20:18:18 +0200

    clearing up

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,19 +5,19 @@ import prettierPlugin from "eslint-plugin-prettier";
 import { defineConfig } from "eslint/config";
 
 export default defineConfig([
-  {
-    files: ["**/*.{js,mjs,cjs,ts}"],
-    plugins: {
-      prettier: prettierPlugin,
-      js: js,
+    {
+        files: ["**/*.{js,mjs,cjs,ts}"],
+        plugins: {
+            prettier: prettierPlugin,
+            js: js,
+        },
+        languageOptions: {
+            globals: globals.node,
+        },
+        rules: {
+            "prettier/prettier": ["error", { tabWidth: 4 }],
+        },
+        extends: ["js/recommended"],
     },
-    languageOptions: {
-      globals: globals.node,
-    },
-    rules: {
-      "prettier/prettier": "error",
-    },
-    extends: ["js/recommended"],
-  },
-  tseslint.configs.recommended,
+    tseslint.configs.recommended,
 ]);
--- main.ts
+++
@@ -1,9 +0,0 @@
-function greet(name: string) {
-  return "asdsd" + name.toUpperCase();
-}
-
-const a = "2";
-
-console.log(a + greet(a));
-
-// dads asdsad
